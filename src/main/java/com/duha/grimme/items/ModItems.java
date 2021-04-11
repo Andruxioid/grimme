252e5e5eea41e70c3b3f1a8fe5f46eb2fa16764c
Andrey <41327680+Andruxioid@users.noreply.github.com>
2021-04-11 15:50:11 +0300

    Added oruta relic and other relic bases

--- src/main/java/com/duha/grimme/items/ModItems.java
+++ src/main/java/com/duha/grimme/items/ModItems.java
@@ -7,15 +7,30 @@ import net.minecraftforge.registries.IForgeRegistry;
 public class ModItems {
 
     public static ItemBase aegurRelic = new ItemBase("aegur_relic").setCreativeTab(CreativeTabs.MISC);
+    public static ItemBase orutaRelic = new ItemBase("oruta_relic").setCreativeTab(CreativeTabs.MISC);
+    public static ItemBase gerretRelic = new ItemBase("gerret_relic").setCreativeTab(CreativeTabs.MISC);
+    public static ItemBase maganRelic = new ItemBase("magan_relic").setCreativeTab(CreativeTabs.MISC);
 
     public static void register(IForgeRegistry<Item> registry) {
         registry.registerAll(
                 aegurRelic
         );
+        registry.registerAll(
+                orutaRelic
+        );
+        registry.registerAll(
+                gerretRelic
+        );
+        registry.registerAll(
+                maganRelic
+        );
     }
 
     public static void registerModels() {
         aegurRelic.registerItemModel();
+        orutaRelic.registerItemModel();
+        gerretRelic.registerItemModel();
+        maganRelic.registerItemModel();
     }
 
 }
